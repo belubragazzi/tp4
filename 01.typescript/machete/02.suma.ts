18fdd2a6231dbc5d93da66eb84947a94af99f27a
Belén Bragazzi <belubragazzi@gmail.com>
2024-06-24 17:14:11 -0300

    solo lo necesario

--- 01.typescript/machete/02.suma.ts
+++
@@ -1,7 +0,0 @@
-function sumar(a: number, b: number): number {
-    return a + b;
-}
-
-console.log(sumar(2, 3));
-
-console.log(sumar(undefined, 3));
--- 09.sysadmin/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Print the container's IP address
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-echo "Container corriendo en: $IP_ADDRESS"
-
-# Keep the container running
-while true; do sleep 86400; done
